888e34d2c0abcb10bf9116a55412f5083842cd51
nkd10121 <nakada10121@icloud.com>
2023-11-22 18:47:46 +0900

    up

---
+++ test/Assets/Boss/Boss1/SphereSensor.cs
@@ -0,0 +1,61 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public class SphereSensor : MonoBehaviour
+{
+    [SerializeField]
+    private SphereCollider searchArea = default;
+    [SerializeField]
+    private float searchAngle = 45f;
+    private LayerMask obstacleLayer = default;
+    private BossController enemyMove = default;
+
+    private void Start()
+    {
+        enemyMove = transform.parent.GetComponent<BossController>();
+    }
+
+    private void OnTriggerStay(Collider target)
+    {
+        if (target.gameObject.tag == "Player")
+        {
+            var playerDirection = target.transform.position - transform.position;
+
+            var angle = Vector3.Angle(transform.forward, playerDirection);
+
+            if (angle <= searchAngle)
+            {
+                //obstacleLayer = LayerMask.GetMask("Block", "Wall");
+
+                if (!Physics.Linecast(transform.position + Vector3.up, target.transform.position + Vector3.up, obstacleLayer))　//プレイヤーとの間に障害物がないとき
+                {
+                    if (Vector3.Distance(target.transform.position, transform.position) <= searchArea.radius * 0.5f
+                        && Vector3.Distance(target.transform.position, transform.position) >= searchArea.radius * 0.05f)
+                    {
+                        enemyMove.SetState(BossController.EnemyState.Attack);
+                    }
+                    else if (Vector3.Distance(target.transform.position, transform.position) <= searchArea.radius
+                        && Vector3.Distance(target.transform.position, transform.position) >= searchArea.radius * 0.5f
+                        && enemyMove.state == BossController.EnemyState.Idle)
+                    {
+                        enemyMove.SetState(BossController.EnemyState.Chase, target.transform); // センサーに入ったプレイヤーをターゲットに設定して、追跡状態に移行する。
+                    }
+                }
+            }
+            else if (angle > searchAngle)
+            {
+                enemyMove.SetState(BossController.EnemyState.Idle);
+            }
+        }
+    }
+#if UNITY_EDITOR
+    //　サーチする角度表示
+    private void OnDrawGizmos()
+    {
+        Handles.color = Color.red;
+        Handles.DrawSolidArc(transform.position, Vector3.up, Quaternion.Euler(0f, -searchAngle, 0f) * transform.forward, searchAngle * 2f, searchArea.radius);
+    }
+#endif
+}
